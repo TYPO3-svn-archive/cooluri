80977c4d8040808d62aaa68653b1aaf6d87c3d54
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-07-09 10:56:25 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@5892 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['linkData-PostProc']['cooluri'] = 'EXT:cooluri/class.tx_cooluri.php:&tx_cooluri->params2cool';
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc']['cooluri'] = 'EXT:cooluri/class.tx_cooluri.php:&tx_cooluri->cool2params';
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['isOutputting']['cooluri'] = 'EXT:cooluri/class.tx_cooluri.php:&tx_cooluri->goForRedirect';
+
+?>
