0e089823b533dea2c7f50baf724c573e9f128c8b
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-07-20 17:19:21 +0000

    Version 1.0, finally! Now you can remove pages (sysfolders) from uri by checking a checkbox. And a few errors fixed.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@5998 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "cooluri"
 #
-# Auto generated 18-07-2007 10:33
+# Auto generated 20-07-2007 19:18
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.11',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"a7eb";s:10:"README.txt";s:4:"091f";s:20:"class.tx_cooluri.php";s:4:"1d99";s:21:"ext_conf_template.txt";s:4:"d485";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2251";s:14:"ext_tables.php";s:4:"280c";s:14:"ext_tables.sql";s:4:"5ad5";s:16:"locallang_db.php";s:4:"935d";s:31:"cooluri/CoolUriConf.xml_default";s:4:"3a17";s:21:"cooluri/XMLSchema.xml";s:4:"cc6c";s:19:"cooluri/link.DB.php";s:4:"be9f";s:24:"cooluri/link.DBLayer.php";s:4:"8c37";s:26:"cooluri/link.Exception.php";s:4:"53de";s:26:"cooluri/link.Functions.php";s:4:"5765";s:21:"cooluri/link.Main.php";s:4:"4850";s:26:"cooluri/link.Translate.php";s:4:"cde5";s:36:"cooluri/manager/linkmanager.Main.php";s:4:"b4d3";s:40:"cooluri/manager/linkmanager.UserAuth.php";s:4:"bde4";s:14:"doc/manual.sxw";s:4:"efbe";s:19:"doc/wizard_form.dat";s:4:"6313";s:20:"doc/wizard_form.html";s:4:"79f3";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"2226";s:14:"mod1/index.php";s:4:"c878";s:18:"mod1/locallang.xml";s:4:"9381";s:22:"mod1/locallang_mod.xml";s:4:"83c2";s:19:"mod1/moduleicon.gif";s:4:"8074";s:14:"mod1/style.css";s:4:"26b2";s:24:"mod1/img/button_edit.gif";s:4:"3248";s:27:"mod1/img/button_garbage.gif";s:4:"90c6";s:27:"mod1/img/button_refresh.gif";s:4:"5178";}',
+	'_md5_values_when_last_written' => 'a:33:{s:9:"ChangeLog";s:4:"a7eb";s:10:"README.txt";s:4:"091f";s:20:"class.tx_cooluri.php";s:4:"54e1";s:21:"ext_conf_template.txt";s:4:"d485";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2251";s:14:"ext_tables.php";s:4:"1bc4";s:14:"ext_tables.sql";s:4:"6adc";s:25:"ext_tables_static+adt.sql";s:4:"356c";s:16:"locallang_db.php";s:4:"a8a7";s:31:"cooluri/CoolUriConf.xml_default";s:4:"3a17";s:21:"cooluri/XMLSchema.xml";s:4:"cc6c";s:19:"cooluri/link.DB.php";s:4:"be9f";s:24:"cooluri/link.DBLayer.php";s:4:"8c37";s:26:"cooluri/link.Exception.php";s:4:"53de";s:26:"cooluri/link.Functions.php";s:4:"5765";s:21:"cooluri/link.Main.php";s:4:"4850";s:26:"cooluri/link.Translate.php";s:4:"cde5";s:36:"cooluri/manager/linkmanager.Main.php";s:4:"b4d3";s:40:"cooluri/manager/linkmanager.UserAuth.php";s:4:"bde4";s:14:"doc/manual.sxw";s:4:"efbe";s:19:"doc/wizard_form.dat";s:4:"6313";s:20:"doc/wizard_form.html";s:4:"79f3";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"2226";s:14:"mod1/index.php";s:4:"c878";s:18:"mod1/locallang.xml";s:4:"9381";s:22:"mod1/locallang_mod.xml";s:4:"83c2";s:19:"mod1/moduleicon.gif";s:4:"8074";s:14:"mod1/style.css";s:4:"26b2";s:24:"mod1/img/button_edit.gif";s:4:"3248";s:27:"mod1/img/button_garbage.gif";s:4:"90c6";s:27:"mod1/img/button_refresh.gif";s:4:"5178";}',
 	'suggests' => array(
 	),
 );
