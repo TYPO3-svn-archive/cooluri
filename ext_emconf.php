4b58ff6d4005c1bab71e44abfd1117c04b2b57cc
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-08 22:11:39 +0000

    Version update
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@18868 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "cooluri"
 #
-# Auto generated 24-06-2008 23:28
+# Auto generated 06-04-2009 21:59
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.15',
+	'version' => '1.0.16',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"a7eb";s:10:"README.txt";s:4:"091f";s:20:"class.tx_cooluri.php";s:4:"b7a9";s:20:"class.tx_cooluri.zip";s:4:"057a";s:21:"ext_conf_template.txt";s:4:"9670";s:12:"ext_icon.gif";s:4:"4b29";s:17:"ext_localconf.php";s:4:"2251";s:14:"ext_tables.php";s:4:"f1cd";s:14:"ext_tables.sql";s:4:"6adc";s:25:"ext_tables_static+adt.sql";s:4:"22bc";s:16:"locallang_db.php";s:4:"b3c0";s:31:"cooluri/CoolUriConf.xml_default";s:4:"705e";s:21:"cooluri/XMLSchema.xml";s:4:"1cc2";s:19:"cooluri/link.DB.php";s:4:"be9f";s:24:"cooluri/link.DBLayer.php";s:4:"8c37";s:26:"cooluri/link.Exception.php";s:4:"53de";s:26:"cooluri/link.Functions.php";s:4:"de06";s:21:"cooluri/link.Main.php";s:4:"4850";s:26:"cooluri/link.Translate.php";s:4:"b7bb";s:36:"cooluri/manager/linkmanager.Main.php";s:4:"aa8c";s:40:"cooluri/manager/linkmanager.UserAuth.php";s:4:"bde4";s:14:"doc/manual.sxw";s:4:"3424";s:19:"doc/wizard_form.dat";s:4:"6313";s:20:"doc/wizard_form.html";s:4:"79f3";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"598a";s:14:"mod1/index.php";s:4:"7679";s:18:"mod1/locallang.xml";s:4:"9381";s:22:"mod1/locallang_mod.xml";s:4:"83c2";s:19:"mod1/moduleicon.gif";s:4:"8bf5";s:14:"mod1/style.css";s:4:"4e67";s:24:"mod1/img/button_edit.gif";s:4:"3248";s:27:"mod1/img/button_garbage.gif";s:4:"90c6";s:27:"mod1/img/button_refresh.gif";s:4:"5178";s:26:"mod1/img/button_sticky.gif";s:4:"b33f";}',
+	'_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"4a71";s:10:"README.txt";s:4:"cfff";s:20:"class.tx_cooluri.php";s:4:"b0f8";s:21:"ext_conf_template.txt";s:4:"8812";s:12:"ext_icon.gif";s:4:"4b29";s:17:"ext_localconf.php";s:4:"2251";s:17:"ext_tables-pg.sql";s:4:"cc6d";s:14:"ext_tables.php";s:4:"b85e";s:14:"ext_tables.sql";s:4:"2460";s:25:"ext_tables_static+adt.sql";s:4:"0b15";s:16:"locallang_db.php";s:4:"b3c0";s:31:"cooluri/CoolUriConf.xml_default";s:4:"1329";s:21:"cooluri/XMLSchema.xml";s:4:"1cc2";s:19:"cooluri/link.DB.php";s:4:"8121";s:24:"cooluri/link.DBLayer.php";s:4:"3972";s:26:"cooluri/link.Exception.php";s:4:"dc79";s:26:"cooluri/link.Functions.php";s:4:"e900";s:21:"cooluri/link.Main.php";s:4:"0e1c";s:26:"cooluri/link.Translate.php";s:4:"d810";s:36:"cooluri/manager/linkmanager.Main.php";s:4:"49a4";s:40:"cooluri/manager/linkmanager.UserAuth.php";s:4:"cc97";s:14:"doc/manual.sxw";s:4:"7bbe";s:19:"doc/wizard_form.dat";s:4:"6313";s:20:"doc/wizard_form.html";s:4:"fb6f";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"598a";s:14:"mod1/index.php";s:4:"ecd5";s:18:"mod1/locallang.xml";s:4:"4a0d";s:22:"mod1/locallang_mod.xml";s:4:"2d90";s:19:"mod1/moduleicon.gif";s:4:"8bf5";s:14:"mod1/style.css";s:4:"4e67";s:24:"mod1/img/button_edit.gif";s:4:"3248";s:27:"mod1/img/button_garbage.gif";s:4:"90c6";s:27:"mod1/img/button_refresh.gif";s:4:"5178";s:26:"mod1/img/button_sticky.gif";s:4:"b33f";}',
 	'suggests' => array(
 	),
 );
