0672f570a4c7a11d6bcdce461aac6a858a929e73
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-03 11:04:02 +0000

    Commiting missing SQL file for the Exclude always feature
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@28314 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,5 +3,6 @@
 #
 CREATE TABLE pages (
 	tx_realurl_pathsegment varchar(30) default '',
-	tx_cooluri_exclude tinyint(1) unsigned default '0'
-); 
+	tx_cooluri_exclude tinyint(1) unsigned default '0',
+	tx_cooluri_excludealways tinyint(1) unsigned default '0'
+);
