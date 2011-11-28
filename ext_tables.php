358e2b8f18251048a95bb251578215d279c655c1
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-28 20:55:48 +0000

    #31732: Added speaking URL to page language overlay, added validation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@55060 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -13,6 +13,7 @@ $TCA['pages']['columns']['tx_realurl_pathsegment'] = array(
 		'type' => 'input',
 		'size' => '30',
 		'max' => '30',
+        'eval' => 'trim,nospace,lower,unique'
 	)
 ); 
 
@@ -32,9 +33,22 @@ $TCA['pages']['columns']['tx_cooluri_excludealways'] = array(
 		'type' => 'check',
 		'default' => '0' 
 	)
-); 
+);
+
+$TCA['pages_language_overlay']['columns']['tx_realurl_pathsegment'] = array(
+        'label' => 'LLL:EXT:cooluri/locallang_db.php:pages.tx_cooluri_pathsegment',
+        'config' => Array (
+                'type' => 'input',
+                'size' => '30',
+                'max' => '30',
+                'eval' => 'trim,nospace,lower,unique'
+        )
+);
 
 t3lib_extMgm::addToAllTCAtypes('pages','tx_realurl_pathsegment,tx_cooluri_exclude,tx_cooluri_excludealways', (t3lib_div::compat_version('4.2') ? '1' : '2'), 'after:nav_title');
 t3lib_extMgm::addToAllTCAtypes('pages','tx_realurl_pathsegment,tx_cooluri_exclude,tx_cooluri_excludealways', (t3lib_div::compat_version('4.2') ? '' : '1,5,') . '4,254', 'after:nav_title');
+
+t3lib_extMgm::addToAllTCAtypes('pages_language_overlay','tx_realurl_pathsegment', (t3lib_div::compat_version('4.2') ? '1' : '2'), 'after:nav_title');
+t3lib_extMgm::addToAllTCAtypes('pages_language_overlay','tx_realurl_pathsegment', (t3lib_div::compat_version('4.2') ? '' : '1,5,') . '4,254', 'after:nav_title');
 
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,10 @@ CREATE TABLE pages (
 	tx_cooluri_exclude tinyint(1) unsigned default '0',
 	tx_cooluri_excludealways tinyint(1) unsigned default '0'
 );
+
+#
+# Modifying pages_language_overlay table
+#
+CREATE TABLE pages_language_overlay (
+	tx_realurl_pathsegment varchar(255) DEFAULT '' NOT NULL
+);
