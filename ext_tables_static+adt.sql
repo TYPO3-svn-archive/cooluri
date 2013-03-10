8300c687343e9854a982ead1d2679afd15f56bd0
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-10 16:46:52 +0000

    Bug #44729 fix - key length
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@72613 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables_static+adt.sql
+++ ext_tables_static+adt.sql
@@ -1,9 +1,6 @@
 
 #
 # Table structure for table 'link_cache'
-# (c) 2007 Jan Bednarik, info@bednarik.org
-# (c) 2008 Tomas J Stehlik, tomas@stehlik.sk
-#
 DROP TABLE IF EXISTS link_cache;
 CREATE TABLE `link_cache` (
 	`id` int(10) unsigned NOT NULL auto_increment,
@@ -14,15 +11,12 @@ CREATE TABLE `link_cache` (
 	`sticky` tinyint(1) unsigned default 0,
 
 	PRIMARY KEY (`id`),
-	KEY `url` (`url`(64)),
-	KEY `params` (`params`(64))
+	KEY `url` (`url`(255)),
+	KEY `params` (`params`(255))
 ) ENGINE = MyISAM;
 
 #
 # Table structure for table 'link_oldlinks'
-# (c) 2007 Jan Bednarik, info@bednarik.org
-# (c) 2008 Tomas J Stehlik, tomas@stehlik.sk
-#
 DROP TABLE IF EXISTS link_oldlinks;
 CREATE TABLE `link_oldlinks` (
 	`id` int(10) unsigned NOT NULL auto_increment,
@@ -32,5 +26,5 @@ CREATE TABLE `link_oldlinks` (
 
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `id` (`id`),
-	KEY `url` (`url`(64))
+	KEY `url` (`url`(255))
 ) ENGINE = MyISAM;
