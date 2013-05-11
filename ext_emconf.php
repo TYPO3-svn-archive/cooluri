bb5e6556835f66283e47fc78afb6b5a58f7697dd
bednee <bednee@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-11 10:01:38 +0000

    CoolUri 1.0.33 release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cooluri/trunk@75328 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "cooluri".
-#
-# Auto generated 20-02-2013 21:45
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cooluri".
+ *
+ * Auto generated 10-05-2013 18:53
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'CoolURI',
 	'description' => 'RealURL alternative. Have nice URLs instead of ugly with parameters. CoolURI has user-friendly XML configuration file. For simple setup, just use the one supplied with extension and you are ready to go.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.32',
+	'version' => '1.0.33',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:38:{s:9:"ChangeLog";s:4:"4a57";s:10:"README.txt";s:4:"cfff";s:20:"class.tx_cooluri.php";s:4:"ebf4";s:21:"ext_conf_template.txt";s:4:"8812";s:12:"ext_icon.gif";s:4:"4b29";s:17:"ext_localconf.php";s:4:"9b09";s:17:"ext_tables-pg.sql";s:4:"08a4";s:14:"ext_tables.php";s:4:"4d2c";s:14:"ext_tables.sql";s:4:"ecc2";s:25:"ext_tables_static+adt.sql";s:4:"0b15";s:16:"locallang_db.xml";s:4:"929e";s:31:"cooluri/CoolUriConf.xml_default";s:4:"db18";s:21:"cooluri/XMLSchema.xml";s:4:"15f2";s:19:"cooluri/link.DB.php";s:4:"8121";s:24:"cooluri/link.DBLayer.php";s:4:"903e";s:26:"cooluri/link.Exception.php";s:4:"dc79";s:26:"cooluri/link.Functions.php";s:4:"46bc";s:20:"cooluri/link.Log.php";s:4:"392b";s:21:"cooluri/link.Main.php";s:4:"d3e8";s:26:"cooluri/link.Translate.php";s:4:"2d0f";s:36:"cooluri/manager/linkmanager.Main.php";s:4:"53d5";s:40:"cooluri/manager/linkmanager.UserAuth.php";s:4:"cc97";s:14:"doc/manual.sxw";s:4:"6718";s:19:"doc/wizard_form.dat";s:4:"6313";s:20:"doc/wizard_form.html";s:4:"fb6f";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"598a";s:14:"mod1/index.php";s:4:"ecd5";s:18:"mod1/locallang.xml";s:4:"4a0d";s:22:"mod1/locallang_mod.xml";s:4:"2d90";s:19:"mod1/moduleicon.gif";s:4:"8bf5";s:14:"mod1/style.css";s:4:"4e67";s:24:"mod1/img/button_edit.gif";s:4:"3248";s:27:"mod1/img/button_garbage.gif";s:4:"90c6";s:27:"mod1/img/button_refresh.gif";s:4:"5178";s:26:"mod1/img/button_sticky.gif";s:4:"b33f";s:20:"test/CoolUriConf.xml";s:4:"6401";s:33:"test/class.tx_cooluritest_pi1.php";s:4:"1e1c";}',
+	'_md5_values_when_last_written' => 'a:44:{s:9:"ChangeLog";s:4:"3b80";s:20:"class.tx_cooluri.php";s:4:"cebb";s:21:"ext_conf_template.txt";s:4:"8812";s:12:"ext_icon.gif";s:4:"4b29";s:17:"ext_localconf.php";s:4:"cdb9";s:17:"ext_tables-pg.sql";s:4:"08a4";s:14:"ext_tables.php";s:4:"4c55";s:14:"ext_tables.sql";s:4:"ecc2";s:25:"ext_tables_static+adt.sql";s:4:"508a";s:16:"locallang_db.xml";s:4:"929e";s:10:"README.txt";s:4:"cfff";s:31:"cooluri/CoolUriConf.xml_default";s:4:"364a";s:19:"cooluri/link.DB.php";s:4:"8121";s:24:"cooluri/link.DBLayer.php";s:4:"903e";s:26:"cooluri/link.Exception.php";s:4:"dc79";s:26:"cooluri/link.Functions.php";s:4:"ed18";s:20:"cooluri/link.Log.php";s:4:"392b";s:21:"cooluri/link.Main.php";s:4:"9990";s:26:"cooluri/link.Translate.php";s:4:"26e8";s:20:"cooluri/link.URI.php";s:4:"f50b";s:21:"cooluri/XMLSchema.xml";s:4:"b0ea";s:36:"cooluri/manager/linkmanager.Main.php";s:4:"53d5";s:40:"cooluri/manager/linkmanager.UserAuth.php";s:4:"cc97";s:14:"doc/manual.sxw";s:4:"30b0";s:19:"doc/wizard_form.dat";s:4:"6313";s:20:"doc/wizard_form.html";s:4:"fb6f";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"598a";s:14:"mod1/index.php";s:4:"ecd5";s:18:"mod1/locallang.xml";s:4:"4a0d";s:22:"mod1/locallang_mod.xml";s:4:"2d90";s:19:"mod1/moduleicon.gif";s:4:"8bf5";s:14:"mod1/style.css";s:4:"4e67";s:24:"mod1/img/button_edit.gif";s:4:"3248";s:27:"mod1/img/button_garbage.gif";s:4:"90c6";s:27:"mod1/img/button_refresh.gif";s:4:"5178";s:26:"mod1/img/button_sticky.gif";s:4:"b33f";s:13:"mod2/conf.php";s:4:"586b";s:14:"mod2/index.php";s:4:"3b12";s:22:"mod2/locallang_mod.xml";s:4:"99d2";s:19:"mod2/moduleicon.gif";s:4:"8bf5";s:33:"test/class.tx_cooluritest_pi1.php";s:4:"6f6b";s:20:"test/CoolUriConf.xml";s:4:"6401";s:23:"test/test.Functions.php";s:4:"05f1";}',
 	'suggests' => array(
 	),
 );
